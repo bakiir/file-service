88038a2b30e7897253fb3ad74008e80386891177
bakiir <bkiranarbek@gmail.com>
2025-08-21 00:08:12 +0600

    Fix: misspell

--- src/test/java/com/example/file_service/service/FileServiceTest.java
+++ src/test/java/com/example/file_service/service/FileServiceTest.java
@@ -1,6 +1,5 @@
 package com.example.file_service.service;
 
-import com.example.file_service.dto.FileDTO;
 import com.example.file_service.mapper.FileMapper;
 import com.example.file_service.model.File;
 import com.example.file_service.repository.FileRepository;
@@ -14,21 +13,19 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.ArgumentMatchers;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.core.io.ByteArrayResource;
 import org.springframework.mock.web.MockMultipartFile;
 import org.springframework.test.util.ReflectionTestUtils;
 
 import java.io.ByteArrayInputStream;
 import java.io.IOException;
-import java.io.InputStream;
 import java.security.InvalidKeyException;
 import java.security.NoSuchAlgorithmException;
 
-import static org.hamcrest.Matchers.in;
 import static org.junit.jupiter.api.Assertions.*;
 
 
-import static org.hamcrest.Matchers.any;
 import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
